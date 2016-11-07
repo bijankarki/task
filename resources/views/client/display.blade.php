089063e25e528ece7f89383efb8ba0a32d838246
Bijan Karki <bijancarkey@gmail.com>
2016-11-07 18:45:49 +0545

    datatable integrated

--- resources/views/client/display.blade.php
+++ resources/views/client/display.blade.php
@@ -5,19 +5,23 @@
 
         <link type="text/css" rel="stylesheet" href="{{asset('assets/bootstrap/css/bootstrap.css')}}" />
         <link type="text/css" rel="stylesheet" href="{{asset('assets/bootstrap/css/bootstrap.min.css')}}" />
+        <link type="text/css" rel="stylesheet" href="{{asset('assets/bootstrap/css/dataTables.bootstrap.min.css')}}" />
         <link type="text/css" rel="stylesheet" href="{{asset('assets/bootstrap/css/bootstrap-theme.css')}}" />
         <link type="text/css" rel="stylesheet" href="{{asset('assets/bootstrap/css/bootstrap-theme-min.css')}}" />
         <link type="text/css" rel="stylesheet" href="{{asset('assets/bootstrap/css/mycss.css')}}" />
     </head>
     <body>
         <div class="col-md-12"id="container">
-            <div class="col-md-11">
+            <div class="col-md-12">
                 <h3>Displaying All Clients</h3>
-                <table class="table-striped" width="100%">
+                <table class="table table-striped table-bordered table-hover" width="100%" id="mydata">
+                    <thead>
                     <tr id="tablehead">
                         <th>S.No.</th><th>Name</th><th>Gender</th><th>Phone</th><th>Email</th><th>Address</th><th>Nationality</th>
                         <th>Dob</th><th>Education Background</th><th>Mode of Contact</th>
                     </tr>
+                  </thead>
+                  <tbody>
                     <?php
                     $i=0;
                     foreach ($arrays as $key => $value ) {
@@ -35,15 +39,18 @@
                       echo '<td>'.$arrays[$key][8].'</td>';
                       echo '</tr>';
                     }
-                    //foreach ($rows as $row){
-                        /*echo "<tr><td>$row[id]</td><td>$row[name]</td><td>$row[work_title]</td><td>$row[duration]</td>"
-                                . "<td>$row[timestamp]</td><td><a href='updatemp.php?id=$row[id]'>update</a> &nbsp;"
-                                . "<a href='../class/delete.php?id=$row[id]'>delete</a></td></tr>";
-                    }*/
                     ?>
+                  </tbody>
         </table>
             </div>
         </div>
+    <script src="{{asset('assets/bootstrap/js/jquery.js')}}"></script>
+    <script src="{{asset('assets/bootstrap/js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('assets/bootstrap/js/dataTables.bootstrap.min.js')}}"></script>
+    <script src="{{asset('assets/bootstrap/js/jquery.dataTables.min.js')}}"></script>
 
+    <script>
+      $('#mydata').DataTable();
+    </script>
 </body>
 </html>
